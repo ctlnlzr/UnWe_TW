53da7c31e7dbc83eda1e498e81dbd63b35b8c8fa
StratianuBianca <bianca.stratianu@info.uaic.ro>
2021-05-29 01:03:02 +0300

    Responsive pages

--- insertData/src/main/java/edu/tw/Main.java
+++ insertData/src/main/java/edu/tw/Main.java
@@ -15,14 +15,13 @@ public class Main {
         VarstaController varsta = new VarstaController();
         MediuController mediu=new MediuController();
         TotalPerLunaController total=new TotalPerLunaController();
-        BufferedReader line = new BufferedReader(new FileReader("C:\\Users\\strat\\Desktop\\rata\\rata15.csv"));
+        BufferedReader line = new BufferedReader(new FileReader("C:\\Users\\strat\\Desktop\\Proiect Tw\\mediu\\medii15.csv"));
         line.readLine(); //fara prima linie;
         String lineText = null;
       while ((lineText = line.readLine()) != null) {
             String[] data = lineText.split(","); //pentru varsta
             String judet = data[0];
-            System.out.println(judet);
-            String[] data3 = lineText.split("\", ");
+            /*(String[] data3 = lineText.split("\", ");
             String[] data2 = lineText.split("\"");
            // String maiMic25 = data[11];
            // StringTokenizer token = new StringTokenizer(maiMic25, " ");
@@ -30,7 +29,13 @@ public class Main {
             String maiMic29 = data2[1];
             String corect=maiMic29.replace(",", "");
             String corect2=corect.replace(" ", "");
-           total.create(15, judet,Integer.parseInt(corect2),  Float.parseFloat(token2.nextToken()));
+           total.create(1, judet,Integer.parseInt(corect2),  Float.parseFloat(token2.nextToken()));
+*/
+          String femeiUrban=data[5];
+          String barbatiUrban=data[6];
+          String femeiRural=data[8];
+          String barbatiRural=data[9];
+          mediu.create(15, judet, Integer.parseInt(femeiUrban), Integer.parseInt(barbatiUrban), Integer.parseInt(femeiRural), Integer.parseInt(barbatiRural));
 
 
         }
